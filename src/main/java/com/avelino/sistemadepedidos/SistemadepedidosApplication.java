40071a15aaad5e9f6add6ad647c492bd208fe21f
carlosavelino <carlos.avelino2.0@gmail.com>
2021-05-24 14:52:04 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/avelino/sistemadepedidos/SistemadepedidosApplication.java
+++ src/main/java/com/avelino/sistemadepedidos/SistemadepedidosApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.avelino.sistemadepedidos.domain.Categoria;
 import com.avelino.sistemadepedidos.domain.Cidade;
+import com.avelino.sistemadepedidos.domain.Cliente;
+import com.avelino.sistemadepedidos.domain.Endereco;
 import com.avelino.sistemadepedidos.domain.Estado;
 import com.avelino.sistemadepedidos.domain.Produto;
+import com.avelino.sistemadepedidos.domain.enuns.TipoCliente;
 import com.avelino.sistemadepedidos.repositories.CategoriaRepository;
 import com.avelino.sistemadepedidos.repositories.CidadeRepository;
+import com.avelino.sistemadepedidos.repositories.ClienteRepository;
+import com.avelino.sistemadepedidos.repositories.EnderecoRepository;
 import com.avelino.sistemadepedidos.repositories.EstadoRepository;
 import com.avelino.sistemadepedidos.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class SistemadepedidosApplication implements CommandLineRunner{
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired 
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemadepedidosApplication.class, args);
@@ -64,6 +73,19 @@ public class SistemadepedidosApplication implements CommandLineRunner{
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "11111111111", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("111111111", "999999999"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "33333333333", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
 	}
 	
 }
---
+++ src/main/java/com/avelino/sistemadepedidos/domain/enuns/TipoCliente.java
@@ -0,0 +1,46 @@
+package com.avelino.sistemadepedidos.domain.enuns;
+
+public enum TipoCliente {
+	
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private int cod;
+	private String descrecao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descrecao = descricao;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public String getDescrecao() {
+		return descrecao;
+	}
+
+	public void setDescrecao(String descrecao) {
+		this.descrecao = descrecao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		
+		if(cod == null) {
+			return null;
+		}
+		
+		for(TipoCliente x : TipoCliente.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
