669fb02b970db536717035d74542f6acea646b60
carlosavelino <carlos.avelino2.0@gmail.com>
2021-06-02 21:42:33 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/avelino/sistemadepedidos/domain/PagamentoComBoleto.java
+++ src/main/java/com/avelino/sistemadepedidos/domain/PagamentoComBoleto.java
@@ -5,12 +5,16 @@ import java.util.Date;
 import javax.persistence.Entity;
 
 import com.avelino.sistemadepedidos.domain.enuns.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataVenciemtno;
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
