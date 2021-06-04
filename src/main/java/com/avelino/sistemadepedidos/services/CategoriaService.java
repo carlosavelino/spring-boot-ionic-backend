e70e68ab2190c0b7224d307447c2640c72108b02
carlosavelino <carlos.avelino2.0@gmail.com>
2021-06-04 20:53:52 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/avelino/sistemadepedidos/services/CategoriaService.java
+++ src/main/java/com/avelino/sistemadepedidos/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.avelino.sistemadepedidos.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.avelino.sistemadepedidos.domain.Categoria;
 import com.avelino.sistemadepedidos.repositories.CategoriaRepository;
+import com.avelino.sistemadepedidos.services.exceptions.DataIntegrityException;
 import com.avelino.sistemadepedidos.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,4 +32,14 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+
 }
---
+++ src/main/java/com/avelino/sistemadepedidos/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,13 @@
+package com.avelino.sistemadepedidos.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	public DataIntegrityException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
