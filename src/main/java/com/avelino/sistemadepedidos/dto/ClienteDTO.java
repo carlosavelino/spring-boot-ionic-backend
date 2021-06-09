1b24ae2701fc5d4369daa8db86e83a37df03d9d9
carlosavelino <carlos.avelino2.0@gmail.com>
2021-06-09 15:34:58 -0300

    Validação customizada: email não repetido na atualização do cliente

--- src/main/java/com/avelino/sistemadepedidos/dto/ClienteDTO.java
+++ src/main/java/com/avelino/sistemadepedidos/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.avelino.sistemadepedidos.domain.Cliente;
+import com.avelino.sistemadepedidos.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
